dc30040fab99893a82001597327572f54af9d684
SergeyNoritsyn <snoritsyn317@gmail.com>
2025-09-22 12:10:38 -0400

    Added form asking user's name

--- hello.py
+++ hello.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'You will never guess this!'
 bootstrap = Bootstrap(app)
 from datetime import datetime, timezone
 from flask_moment import Moment
 moment = Moment(app)
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
-@app.route('/') # specifies URL path/endpoint that triggers this function
-def index(): # function that runs when the endpoint is accessed
-    return render_template('index.html') # returns HTML content in a Jinja2 template
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST']) # allow both GET and POST requests
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>') # dynamic route with variable component
 def user(name):
