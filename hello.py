0c0e9988f7c19f01eca3d83de20babbb637002d5
SergeyNoritsyn <snoritsyn317@gmail.com>
2025-09-22 10:54:45 -0400

    Commented basic Flask application

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<h1>Hello World!</h1>'
+@app.route('/') # specifies URL path/endpoint that triggers this function
+def index(): # function that runs when the endpoint is accessed
+    return '<h1>Hello World!</h1>' # returns HTML content/response
